970fb01627df5432e168e7e65462c29b5ba7e4f5
KmancusoRonik <105605229+KmancusoRonik@users.noreply.github.com>
2023-06-14 20:30:14 -0400

    Version Upgrade v.1.0.5.95
    re-enable the github token convert repo to private repo.

--- ronikdesign.php
+++ ronikdesign.php
@@ -56,7 +56,7 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 //Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('main');
 //Optional: If you're using a private repository, specify the access token like this:
-// $myUpdateChecker->setAuthentication('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');
+$myUpdateChecker->setAuthentication('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');
 
 
 
